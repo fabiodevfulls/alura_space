14d906626d9ac73900ad365b214f7a75485b8492
fabiodevfulls <fabio.boy.2010.fs@gmail.com>
2023-03-23 13:13:57 -0300

    partials

--- car/views.py
+++ car/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    return render(request, 'galeria/index.html')
+
 
 def imagem(request):
-    return render(request, 'imagem.html')
+    return render(request, 'galeria/imagem.html')
 
 
 
